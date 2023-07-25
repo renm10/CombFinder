cb4147bee9940267f20fe8e051fbb5eb40ef8b68
renm10 <43124584+renm10@users.noreply.github.com>
2023-07-24 21:15:40 -0700

    Finished algorithm, added comments and readme

--- Node.h
+++ Node.h
@@ -1,4 +1,15 @@
 struct Node {
     Node* parent;
     int num;
+
+    Node* createChild(int number);
 };
+
+Node* Node::createChild(int number)
+{
+    Node* newNode = new Node;
+    newNode->parent = this;
+    newNode->num = number;
+
+    return newNode;
+}
--- main.cpp
+++ main.cpp
@@ -4,60 +4,57 @@
 
 using namespace std;
 
+void algorithm(Node* root, vector<int> list, int total, const int combtotal);
 void printCombination(Node*);
 
 int main()
 {
     vector<int> list;
     list.push_back(1);
-    list.push_back(9);
+    list.push_back(2);
     list.push_back(3);
-    // list.push_back(4);
+    list.push_back(4);
 
-    int total;
-    int combtotal = 10;
-    bool found = false;
+    int total = 0;
+    int combtotal = 3;
     
     for (int i = 0; i < list.size(); i++)
     {
-        Node* tail; // Keeps track of the tail
-        Node* root = new Node;
+        Node* root = new Node; //Create root node for each number in the list
         root->num = list[i];
-        root->parent = NULL;
-        tail = root;
-        total = 0;
+        root->parent = NULL; 
         total += root->num;
-        cout << total << endl;
 
-        while (!found)
-        {
-            for (int j = 0; j < list.size(); j++)
-            {
-                Node* newnode = new Node;
-                newnode->num = list[j];
-                newnode->parent = tail;
-                tail = newnode;
-                if (total == combtotal)
-                {
-                    found = true;
-                    cout << "Combination Found for:"  << list[i] << endl;
-                    printCombination(tail);
-                }
-                
-                total += newnode->num;
-                cout << total << endl;
+        algorithm(root, list, total, combtotal);
 
-                if (total > combtotal)
-                {
-                    //cout << "BREAK" << endl;
-                    break;
-                }
-            }
-        }
-        found = false;
+        total = 0;
     }
 }
 
+/*Recursive Algorithm to list all possible patterns that add to a certain sum */
+void algorithm(Node* root, vector<int> list, int total, const int combtotal)
+{
+    for (int i = 0; i < list.size(); i++)
+    {
+        Node* newNode = root->createChild(list[i]); //Create the child 
+        total += newNode->num;
+
+        if (total == combtotal) //Found a combination
+        {
+            cout << "Combination Found" << endl;
+            printCombination(newNode);
+            cout << endl;
+        }
+        else if (total < combtotal) //Only go further if the total hasn't surpassed the actual total we want
+        {
+            algorithm(newNode, list, total, combtotal);
+        }
+
+        total -= newNode->num; //Subtract the amount added because new loop means new child
+    }
+}
+
+//Starting from the leaf node, prints out the combination. 
 void printCombination(Node* leaf)
 {
     if (leaf == NULL)
